9a6104264d927c28e4b5c6f83bd5767d00c48b10
aishwarya-stack <aishwaryaashokkadam@gmail.com>
2021-09-19 11:06:31 +0530

    ADD:odd even program added

---
+++ oddEven.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class oddEven {
+
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println("Enter the no");
+  Scanner sc=new Scanner(System.in);
+   int number=sc.nextInt();
+   
+   if(number%2==0)
+   {
+	   System.out.println("even no");
+   }
+   else
+   {
+	   System.out.println("odd no");
+   }
+}
+
+}
