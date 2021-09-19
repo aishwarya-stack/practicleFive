ca02b5a9587785422e0c9a1a9b737dd214f5b718
aishwarya-stack <aishwaryaashokkadam@gmail.com>
2021-09-19 11:03:15 +0530

    ADD:prime no program added

---
+++ Prime.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Prime {
+
+	public static void main(String[] arg)
+	{
+		 int number;
+	      Scanner sc = new Scanner(System.in);
+	      System.out.println("Enter a number ::");
+	      number = sc.nextInt();
+	     
+	      for(int i = 2; i< number; i++) {
+	         while(number%i == 0) {
+	            System.out.println(i+" ");
+	            number = number/i;
+	         }
+	      }
+	      if(number >2) {
+	         System.out.println(number);
+	      }
+	}
+}
