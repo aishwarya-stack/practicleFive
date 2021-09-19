be1fe0e9f9385716b81bf4f79e99233e2522d57b
aishwarya-stack <aishwaryaashokkadam@gmail.com>
2021-09-19 11:08:20 +0530

    ADD:swapping program added

---
+++ swap.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class swap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int number1=10,number2=20,temp;
+     temp=number1;
+     number1=number2;
+     number2=temp;
+     System.out.println("no after swapping value of number1:"+number1);
+     System.out.println("no after swapping value of number2:"+number2);
+	}
+
+}
